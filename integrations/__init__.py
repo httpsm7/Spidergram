e67ba151ac7ddbb32e98df788608fad02bdb9b55
Sharlix <http.m7@gmail.com>
2026-05-15 15:09:04 +0530

    Add files via upload

---
+++ integrations/__init__.py
@@ -0,0 +1,6 @@
+from .newsapi    import fetch_top_headlines, fetch_everything  # noqa
+from .gnews      import fetch_top_news                         # noqa
+from .pexels     import search_photos, search_videos, get_best_video_file, get_photo_url  # noqa
+from .elevenlabs import generate_speech                        # noqa
+from .grok       import chat as grok_chat                      # noqa
+from .instagram  import publish_image, publish_reel, get_insights  # noqa
